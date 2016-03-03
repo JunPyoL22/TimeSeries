705be5680cb1ad51844abc0198250084b2ffc714
JunPyoL22 <jplee031107@gmail.com>
2016-03-03 23:33:53 +0900

    with error

--- bootstrap3.py
+++ bootstrap3.py
@@ -2,16 +2,16 @@
 import numpy as np
 
 n =100
-bootstrapSample = np.random.randn(n,1)
-mu=np.mean(bootstrapSample)
+iidSample = np.random.randn(n,1)
+mu=np.mean(iidSample)
 B= 1000
 muStar = np.zeros((n,1))
-bootstrapSample = np.sort(bootstrapSample)
+iidSample = np.sort(iidSample)
 
 for i in range(0,B,2):
     index = np.random.randint(n,size=(n,))
-    muStar[i] = np.mean(bootstrapSample[index])
-    index = n-index
-    muStar[i+1] = np.mean(bootstrapSample[index])
+    muStar[i] = np.mean(iidSample[index])
+    index = (n-1)-index
+    muStar[i+1] = np.mean(iidSample[index])
 
 np.correlate(muStar[list(range(0,B,2))],muStar[range(1,B,2)])
