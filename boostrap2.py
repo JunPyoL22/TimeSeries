086a94045f70812a83bcfb3c0fdf25d016357573
JunPyoL22 <jplee031107@gmail.com>
2016-03-03 22:48:49 +0900

    IID
    IID Balanced Boostrap

--- boostrap2.py
+++ boostrap2.py
@@ -1,15 +1,20 @@
-import numpy as np
-# import matplotlib.pyplot as plt
+"""IID Balanced Boostrap"""
 
-n = 100
-iidSample = np.random.randn(n,1)
+import numpy as np
+
+n=100
+B=1000
+iidSample=np.random.randn(n,1)
 mu = np.mean(iidSample)
-B = 1000
 muStar = np.zeros((B,1))
+iidSampleRepl = np.tile(iidSample,(B,1))
+u = np.random.permutation(n*B)
+
 for i in range(B):
-    order = list(np.random.randint(n,size=(n,)))
-    boostrapSample = iidSample[order]
+    index = list(n*(i)+np.arange(0,n))
+    boostrapSample = iidSampleRepl[u[index]]
     muStar[i] = np.mean(boostrapSample)
+
 s2 = np.sum((iidSample-mu)**2)*(1/(n-1))
 stdErr = s2/n
 boostrapStdErr = np.mean((muStar-mu)**2)
